d7c8c865e028ac88acaf23754a2c44dfd2fba557
Rodrj1 <rodrigo_mdq93@hotmail.com>
2023-08-26 19:53:32 -0300

    Implemented filters for projects

--- src/lib/projectData/projectsPreview.ts
+++ src/lib/projectData/projectsPreview.ts
@@ -10,36 +10,42 @@ export const projectsPreview = [
 		image:
 			'https://cdn.sanity.io/images/8nn8fua5/production/0e11b9e81dbff0e91cdf326ac726a1b28233636f-870x582.jpg?w=720&fm=webp&q=65',
 		imageAlt: 'The Webby Awards Logo',
-		link: 'https://basicagencymock.netlify.app/'
+		link: 'https://basicagencymock.netlify.app/',
+		type: 'Design'
 	},
 	{
 		name: 'Dua Lipa Albums',
 		image: dualipa,
 		imageAlt: 'Dua Lipa Concept two albums.',
-		link: 'https://dualipaconcept.netlify.app/'
+		link: 'https://dualipaconcept.netlify.app/',
+		type: 'Design'
 	},
 	{
 		name: 'Forest of An Ylthin',
 		image: anylthin,
 		imageAlt: 'Card Battle from the game of An Ylthin',
-		link: '/projects/anylthin'
+		link: '/projects/anylthin',
+		type: 'Game'
 	},
 	{
 		name: 'ARCFiction',
 		image: arcfiction,
 		imageAlt: 'Home menu featuring multiple tv shows',
-		link: '/projects/arcfiction'
+		link: '/projects/arcfiction',
+		type: 'Website'
 	},
 	{
 		name: 'City of Hithair',
 		image: cemetery,
-		imageAlt: 'Cemetery Levle with skeletons from the game: City of Hithair',
-		link: '/projects/cityofhithair'
+		imageAlt: 'Cemetery Level with skeletons from the game: City of Hithair',
+		link: '/projects/cityofhithair',
+		type: 'Game'
 	},
 	{
 		name: 'Google Custom Design',
 		image: searcher,
 		imageAlt: 'Google Searcher',
-		link: '/projects/googleclone'
+		link: '/projects/googleclone',
+		type: 'Website'
 	}
 ];
--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -1,4 +1,4 @@
-<script>
+<script lang="ts">
 	import { onMount } from 'svelte';
 	import { cognitiveDissabilityMode } from '../../store';
 	import { addBordersForCognitive } from '../components/AccessibilityMenu/cognitiveDissabilityMode';
@@ -8,11 +8,20 @@
 	onMount(() => {
 		if ($cognitiveDissabilityMode === true) addBordersForCognitive($cognitiveDissabilityMode);
 	});
+
+	let displayProjects = projectsPreview;
+
+	const filterBy = (type: string) => {
+		if (type === '') displayProjects = projectsPreview;
+		else {
+			const filteredItems = projectsPreview.filter((news) => news.type === type);
+			displayProjects = filteredItems;
+		}
+	};
 </script>
 
 <section class="w-full mb-40 xl:mb-20">
 	<div class="flex flex-col w-[95%] px-5 m-auto gap-5 border-l border-l-slate-200">
-
 		<div class="w-full flex flex-col gap-3 mb-5">
 			<h2 class="h-auto text-xl sm:text-2xl md:text-3xl lg:text-4xl text-left text-slate-200">
 				Explore
@@ -24,12 +33,31 @@
 				target="_blank"
 				aria-label="See my Github Profile: Rodrj1">Github</a
 			>
+
+			<ul
+				class="flex w-[100%] m-auto gap-14 text-sm font-sans font-medium text-slate-200 spacing overflow-y-hidden overflow-x-scroll sm:overflow-hidden sm:justify-start list-none pl-9 pb-3"
+			>
+				<li>
+					<button on:click={() => filterBy('')} class="filterButton relative"> VIEW ALL </button>
+				</li>
+				<li>
+					<button on:click={() => filterBy('Design')} class="filterButton relative">DESIGNS</button>
+				</li>
+				<li>
+					<button on:click={() => filterBy('Game')} class="filterButton relative">GAMES</button>
+				</li>
+				<li>
+					<button on:click={() => filterBy('Website')} class="filterButton relative"
+						>WEBSITES</button
+					>
+				</li>
+			</ul>
 		</div>
 
 		<ul class="p-0 flex flex-col gap-5 list-none">
-			{#each projectsPreview as projectPreview, index(index)}
+			{#each displayProjects as projectPreview, index (index)}
 				<li class="flex-1">
-					<ProjectPreview {projectPreview} {index}/>
+					<ProjectPreview {projectPreview} {index} />
 				</li>
 			{/each}
 		</ul>
